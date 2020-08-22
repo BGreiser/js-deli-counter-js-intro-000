c8d98fb011844af2468ed571f4aec7fde899f9f8
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-22 21:21:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line, newPerson) {
   let welcome = `Welcome, ${newPerson}. You are number ${line.length} in line.`
   return welcome;
 }
+
+function nowServing(line) {
+  if (line.lenght > 0) {
+    let x = line.shift();
+    return x;
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
